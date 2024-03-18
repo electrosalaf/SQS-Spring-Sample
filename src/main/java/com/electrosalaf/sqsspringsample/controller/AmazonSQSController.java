bf0bd448cad5a95f7434aae3f05cd9c6c98f7799
Ibrahim Lawal <ilawal@digicoreltd.com>
2024-03-18 20:15:59 +0100

    code clean up

--- src/main/java/com/electrosalaf/sqsspringsample/controller/AmazonSQSController.java
+++ src/main/java/com/electrosalaf/sqsspringsample/controller/AmazonSQSController.java
@@ -3,11 +3,9 @@ package com.electrosalaf.sqsspringsample.controller;
 import com.electrosalaf.sqsspringsample.service.AmazonSQSService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
-import org.springframework.cloud.aws.messaging.listener.annotation.SqsListener;
-import org.springframework.messaging.support.MessageBuilder;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author Ibrahim Lawal
@@ -18,9 +16,6 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @Slf4j
 public class AmazonSQSController {
-
-    @Value("${cloud.aws.end-point.uri}")
-    private String awsUri;
 
     private final AmazonSQSService amazonSQSService;
 
